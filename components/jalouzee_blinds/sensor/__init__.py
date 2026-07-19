478be815bcf1effab5413757d018505b42c1d426
shammysha <65885873+shammysha@users.noreply.github.com>
2026-07-19 16:11:20 +0300

    fix enum in sensor/init.py

--- components/jalouzee_blinds/sensor/__init__.py
+++ components/jalouzee_blinds/sensor/__init__.py
@@ -5,10 +5,8 @@ from esphome.components import sensor
 
 from .. import JalouzeeBlinds
 
-
 CONF_PARENT = "parent"
 CONF_TYPE = "type"
-
 
 CONFIG_SCHEMA = sensor.sensor_schema().extend(
     {
@@ -16,13 +14,13 @@ CONFIG_SCHEMA = sensor.sensor_schema().extend(
             cv.use_id(JalouzeeBlinds),
 
         cv.Required(CONF_TYPE):
-            cv.enum(
-                {
-                    "angle",
-                    "position",
-                },
-                lower=True
-            ),
+        cv.enum(
+            {
+                "angle": "angle",
+                "position": "position",
+            },
+            lower=True,
+        ),
     }
 )
 
